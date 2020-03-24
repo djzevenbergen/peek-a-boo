30536a8d89918b27cad219054eb3d153e08cfde1
djzevenbergen <djzevenbergen@gmail.com>
2020-03-24 11:01:29 -0700

    add night/day mode and paragraph highlighting

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,27 @@ $(document).ready(function() {
         if (document.getElementById("walrusButton").innerHTML === "Show Image") {
             document.getElementById("walrusButton").innerHTML = "Hide Image";
         } else {
-          document.getElementById("walrusButton").innerHTML = "Show Image";
+            document.getElementById("walrusButton").innerHTML = "Show Image";
         }
+      });
+
+    $("#darkButton").click(function() {
+        if (document.getElementById("darkButton").innerHTML === "Night Mode") {
+            document.getElementById("darkButton").innerHTML = "Day Mode";
+            $("body").addClass("night-mode");
+        } else {
+          document.getElementById("darkButton").innerHTML = "Night Mode";
+          $("body").removeClass();
+        }
+
+    }); 
+    
+    $("p").click(function() {
+        $("p").toggleClass("highlight");
+
+
     });
+
+
 });
 
