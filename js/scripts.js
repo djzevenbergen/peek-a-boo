fe59d3682c8f36dc0fcd412f450a9f9afc676289
djzevenbergen <djzevenbergen@gmail.com>
2020-03-24 10:34:51 -0700

    Add alternating text for button.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     $("#walrusButton").click(function() {
         $("#initially-showing").fadeToggle();
         $("#initially-hidden").fadeToggle();
-        
+        if (document.getElementById("walrusButton").innerHTML === "Show Image") {
+            document.getElementById("walrusButton").innerHTML = "Hide Image";
+        } else {
+          document.getElementById("walrusButton").innerHTML = "Show Image";
+        }
     });
 });
 
